69ccb4fa59c0af3883782e147430bd75e6c473ca
felix2072 <info@felix-w.de>
2021-06-15 17:08:31 +0200

    delete kill cmd line

--- pull.bat
+++ pull.bat
@@ -11,7 +11,3 @@ start c:\vvvv\vvvv_beta_41_x64\vvvv.exe /allowmultiple /o %~dp0test.v4p
 Timeout 3
 
 start c:\vvvv\vvvv_beta_41_x64\vvvv.exe /allowmultiple /o %~dp0color.v4p
-
-Timeout 3
-
-TASKKILL /F /IM cmd.exe
